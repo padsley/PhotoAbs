88aa87a06f17d0439047a6b2327c44cafbcd6d59
padsley <padsley@gmail.com>
2023-03-08 10:20:19 -0600

    Phil is adding comments

--- PlotData.cpp
+++ PlotData.cpp
@@ -1,45 +1,54 @@
 {
-    
+  //open the input file coming from Dietrich and Berman
     ifstream input;
     input.open("data.txt");
-    
+
+    //make TGraphs to put the results in
     TGraphErrors *ge = new TGraphErrors();
     ge->SetName("polGraph");
     
     TGraphErrors *ge2 = new TGraphErrors();
     ge2->SetName("polGraphReval");
     
-    
+    //also make a function for the Migdal prediction to use
     TF1 *fMig = new TF1("fMig","2.25e-3 * pow(x,5./3.)",1,250);
     
-    
+    //...and make a TGraph to put the values for the ratio into
     TGraphErrors *geRatio = new TGraphErrors();
     geRatio->SetName("polRatioGraph");
-    
+
+    //check the input is open
     if(input.is_open())
     {
+      //declare some things to put values into
         int Z = 0, A = 0;
         double sigma_neg2 = 0, sigma_sigma = 0;
-        
+
+	//while we can successfully read a line with the atomic number, Z, the mass number, A, the dipole pol sigma_neg2 and the uncertainty on that sigma_sigma, keep going
         while(input >> Z >>  A >> sigma_neg2 >> sigma_sigma)
         {
 //             sigma_neg2*=1000; //convert to ub
 //             printf("%d \t %d \t %f \t %f \n",Z,A,sigma_neg2,sigma_sigma);
             
-            ge->SetPoint(ge->GetN(),A,sigma_neg2);
-            
+	  //set the points for the dipole pol graph
+	  ge->SetPoint(ge->GetN(),A,sigma_neg2);
+
+	  //if no uncertainty, assume 10%
             if(sigma_sigma == 0)sigma_sigma = 0.1*sigma_neg2;
-            
+
+	    //set that value
             ge->SetPointError(ge->GetN()-1,0,sigma_sigma);
             
-            
+            //ratio of the result to migdal function
             geRatio->SetPoint(geRatio->GetN(),A,sigma_neg2/fMig->Eval((double)A));
+	    //with associated uncertainty
             geRatio->SetPointError(geRatio->GetN()-1,0,sigma_sigma/fMig->Eval((double)A));
         }
     }
-    
+    //finished reading in the historical file - close it
     input.close();
-    
+
+    //manually set re-evaluated values
     ge2->SetPoint(0,208,19.8);//208Pb
     ge2->SetPoint(1,90,3.69737);//90Zr
     ge2->SetPoint(2,112,5.22459);//112Sn
@@ -48,10 +57,11 @@
     ge2->SetMarkerColor(2);
     
     
-    
+    //make something to plot the resluts on
     TCanvas *c1 = new TCanvas();
     
 //     c1->SetLogx();
+    //set information about the graph e.g. axis labels and formatting
     c1->SetLogy();
     
     ge->Draw("AP");
@@ -72,7 +82,8 @@
     fMig->Draw("same");
     fMig->SetLineStyle(2);
     fMig->SetLineWidth(1);
-    
+
+    //do the same as above for the ratios
     TCanvas *c2 = new TCanvas();
     
     geRatio->Draw("AP");
@@ -89,26 +100,32 @@
     
     geRatio->SetMaximum(2.0);
     geRatio->SetMinimum(0.05);
-    
+
+    //draw some lines showing the closed neutron shells in mass number
+    //40Ca (N=20)
     TLine *line1 = new TLine(40,1,ge->GetPointX(ge->GetN()-1),1);
     line1->SetLineStyle(3);
     line1->Draw("same");
-    
+
+    //90Zr (N=50)
     TLine *line2 = new TLine(90,0.2,90,1.8);
     line2->SetLineStyle(4);
     line2->SetLineColor(4);
     line2->Draw("same");
-    
+
+    //138Ba (N=82)
     TLine *line3 = new TLine(138,0.2,138,1.8);
     line3->SetLineStyle(4);
     line3->SetLineColor(4);
     line3->Draw("same");
-    
+
+    //208Pb (N=208)
     TLine *line4 = new TLine(208,0.2,208,1.8);
     line4->SetLineStyle(4);
     line4->SetLineColor(4);
     line4->Draw("same");
-    
+
+    //do the ratios for the new re-evaluated points to the Migdal formula
     TGraphErrors *ge2Ratio = new TGraphErrors();
     ge2Ratio->SetTitle("ge2Ratio");
     
--- README.md
+++ README.md
@@ -14,3 +14,13 @@ root
 The data.txt file contains the polarisabilities from Dietrich and Bermann
 
 The PlotData.cpp file gives another way of plotting new results
+
+To add things:
+git add -u (will add all changed files to the repo)
+git add <file> (will add a specific file to the repo)
+
+git commit -m "message" (make a commit, like a little package of changes to do all at once, with a message explaining it)
+
+git push (push the changes to the remote repository)
+
+git pull (pull any changes from the remote repository)
